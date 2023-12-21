94fe224f8152118c6e13187ea5a0a226a93d3c7a
Szymon Czajkowski <sczajkowski83@gmail.com>
2023-12-21 21:14:59 +0100

    basic dicover view

--- views/discover.php
+++ views/discover.php
@@ -8,7 +8,50 @@
 </head>
 <body>
     <div class="container">
-        <h1>discover</h1>
+        <nav>
+            <p class="header">TRAINING BUDDY</p>
+            <div class="link-panel">
+                <a href="login" class="login-link">DISCOVER</a>
+                <a href="login" class="login-link">PROFILE</a>
+            </div>
+        </nav>
+        <main>
+            <div class="search-panel">
+                <form>
+                    <input type="text" placeholder="City">
+                </form>
+            </div>
+            <section class="profiles">
+                <div class="profile-card">
+                    <img src="" alt="">
+                    <p>John</p>
+                    <p>Cracow</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, consequuntur.</p>
+                    <button>CONTACT</button>
+                </div>
+                <div class="profile-card">
+                    <img src="" alt="">
+                    <p>John</p>
+                    <p>Cracow</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, consequuntur.</p>
+                    <button>CONTACT</button>
+                </div>
+                <div class="profile-card">
+                    <img src="" alt="">
+                    <p>John</p>
+                    <p>Cracow</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, consequuntur.</p>
+                    <button>CONTACT</button>
+                </div>
+                <div class="profile-card">
+                    <img src="" alt="">
+                    <p>John</p>
+                    <p>Cracow</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, consequuntur.</p>
+                    <button>CONTACT</button>
+                </div>
+            </section>
+        </main>
     </div>
 </body>
 </html>
